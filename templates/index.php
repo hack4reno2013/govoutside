28519b0f23606f845e2790e71ce5ee8052b17dc1
Dawson Loudon <dawson@dawsonloudon.com>
2013-10-06 15:39:34 -0700

    final

--- templates/index.php
+++ templates/index.php
@@ -10,8 +10,9 @@
                         <img src="assets/img/logo_lg.png" id="logo-lg" />
                     </div>
                     <div id="hero-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus, massa id rutrum interdum, nisl leo lobortis lorem, quis sodales nisl enim sed metus. Quisque blandit pretium malesuada. Fusce euismod vel arcu vitae faucibus. Vestibulum consequat nisl ac tristique facilisis. Etiam accumsan justo est, et iaculis sem pharetra sed. Suspendisse vitae euismod enim. Pellentesque iaculis nunc tempus neque luctus volutpat. Nam malesuada, ante vitae lacinia tempor, quam dolor hendrerit mi, ac congue felis orci sit amet augue. Pellentesque ante leo, lobortis vel purus sit amet, tristique tristique neque.</p>
-                        <p>Etiam massa lorem, dignissim id quam et, interdum pulvinar ante. Nulla quis sollicitudin massa, eget fermentum massa.</p>
+                        <p>GovOutside is a simple to use outdoor location mapping widget for government of all sizes!</p>
+                        <p>In three simple steps you can have the widget seen to the left on your website. Start by creating categories, then add locations and finally copy the provided code into a page on your website. That's it!</p>
+                        <p>Anytime you login and make edits, the widget on your site is automatically updated.</p>
                     </div>
                     <div id="starter-btns">
                         <div id="login-btn" onclick="javascript: window.location='?view=users&action=login';"></div>
@@ -19,4 +20,22 @@
                     </div>
                 </div>
                 <div class="clear"></div>
+            </div>
+            <div id="steps-wrapper">
+                <div class="column" id="step1">
+                    <h2>Step1: Create Categories</h2>
+                    <p>Categories are great way to help people find what they are looking for faster. You can use labels like 'Parks' and 'Trails' to describe the type of location or you can use regional labels like 'Downtown' and 'North Valleys' to allow people to search by general local.<p>
+                </div>
+                <div class="column" id="step2">
+                    <h2>Step 2: Add Locations</h2>
+                    <p>Add locations to your various categories and let the people know where to go outside in your area. Parks, Memorials, Hiking Trails, Scenic Lookouts and Historic Sites make great locations to add to your GovOutside widget!</p>
+                </div>
+                <div class="column" id="step3">
+                    <h2>Step 3: Get the Code</h2>
+                    <p>After you create your categories and add your locations you are ready to add your GovOutside widget to your website! Simply copy the code snippet provided and place the into the content area of your page. THAT'S IT! Now, anytime you login and edit your categories or locations your widget instantly updates on your website.</p>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div id="foot">
+                &copy; 2013 GovOutside.com
             </div>

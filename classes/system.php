fb001fe3176f99d64d867a145c45d0bd96b059d1
Chris Crevling <mcduffs.crapfling@gmail.com>
2013-10-06 09:40:39 -0700

    fixing hidden field on category

--- classes/system.php
+++ classes/system.php
@@ -109,7 +109,7 @@ class System extends govOutSide {
 					0 => array( 'label' => '', 'name' => 'uid', 'type' => 'hidden', 'value' => $this->user_id, 'required' => false ),
 					1 => array( 'label' => 'Label', 'name' => 'label', 'value' => @$cur_data['label'], 'type' => 'text', 'required' => true ),
 					2 => array( 'label' => 'Color', 'name' => 'color', 'value' => @$cur_data['color'], 'type' => 'text', 'required' => true ),
-					3 => array( 'label' => '', 'name' => 'catid', 'type' => 'text', 'value' => $cur_id, 'required' => true )
+					3 => array( 'label' => '', 'name' => 'catid', 'type' => 'hidden', 'type' => 'text', 'value' => $cur_id, 'required' => true )
 				);
 			break;
 		}

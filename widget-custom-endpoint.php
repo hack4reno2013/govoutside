377e2554c3e94434393ae07f9bfaabb5b90c8d50
Dawson Loudon <dawson@dawsonloudon.com>
2013-10-06 13:20:36 -0700

    simple revisions

--- widget-custom-endpoint.php
+++ widget-custom-endpoint.php
@@ -2,23 +2,6 @@
 <html lang="en-us">
 	<head>
 
-		<script type="text/javascript">
-
-		(function(d, script) {
-			var api_key = 'ZGF3c29uQGRhd3NvbmxvdWRvbi5jb206RGF3c29uX19Mb3Vkb246NTU3Mw==';
-			var endpoint = '//' + location.host + '/?view=ajax&api_key=' + api_key
-			script = d.createElement('script');
-			script.type = 'text/javascript';
-			script.async = true;
-			script.onload = function(){
-				govOutsideWidget.init(api_key, endpoint);
-			};
-			script.src = 'assets/scripts/widget.js';
-			d.getElementsByTagName('head')[0].appendChild(script);
-		}(document));
-
-		</script>
-
 	</head>
 	<body>
 
@@ -26,6 +9,22 @@
 			<div id="go-root">
 
 			</div>
+					<script type="text/javascript">
+
+            		(function(d, script) {
+            			var api_key = 'ZGF3c29uQGRhd3NvbmxvdWRvbi5jb206RGF3c29uX19Mb3Vkb246NTU3Mw==';
+            			var endpoint = '//' + location.host + '/?view=ajax&api_key=' + api_key
+            			script = d.createElement('script');
+            			script.type = 'text/javascript';
+            			script.async = true;
+            			script.onload = function(){
+            				govOutsideWidget.init(api_key, endpoint);
+            			};
+            			script.src = 'assets/scripts/widget.js';
+            			d.getElementsByTagName('head')[0].appendChild(script);
+            		}(document));
+
+            		</script>
 		</div>
 
 	</body>

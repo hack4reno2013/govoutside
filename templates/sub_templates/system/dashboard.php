6748a4fc370733bda968b6b166d2e5f4cc9f996b
Dawson Loudon <dawson@dawsonloudon.com>
2013-10-05 21:11:34 -0700

    styles for dashboard and editor forms

--- templates/sub_templates/system/dashboard.php
+++ templates/sub_templates/system/dashboard.php
@@ -1,15 +1,29 @@
 <?php
 
 ?>
-
-Dashboard
-
+<h2>Cateogries</h2>
+<div id="categories-area">
 <?php
-
+$categories = $this->getCategories();
+foreach($categories as $category){
+?>
+<div class="category" style="background-color:<?=$category['color']?>;"><?=$category['label']?></div>
+<?php
+}
+?>
+</div>
+<h2>Add Category</h2>
+<div id="categories-form-area">
+<?php
 $categories_form_fields = $this->getFormFields('categories');
 echo $this->formMediator($categories_form_fields, 'categories');
-
+?>
+</div>
+<h2>Locations</h2>
+<h2>Add Location</h2>
+<div id="locations-form-area">
+<?php
 $location_form_fields = $this->getFormFields('location');
 echo $this->formMediator($location_form_fields, 'location');
-
 ?>
+</div>
